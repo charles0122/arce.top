6ffc1b4ed14db7678d603a120214abdc08f39141
charles <zainai311@qq.com>
2024-04-21 18:37:53 +0800

    fix vercel

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -55,7 +55,7 @@ const config: Config = {
   url: 'https://arce.top',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/arcetop',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
